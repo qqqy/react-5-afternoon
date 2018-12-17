baeb4732e32d166eeac61f36d47c7d23f3508010
qqqy <frogasaurus@hotmail.com>
2018-12-17 16:43:40 -0700

    first push, completed step 6

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,4 +1,47 @@
-function reducer(){
+
+const initialState = {
+  loanType: 'Home Purchase',
+  propertyType: 'Single Family Home',
+  city: '',
+  propToBeUsedOn: '',
+  found: "false",
+  cost: 0,
+  downPayment: 0,
+  credit: '',
+  history: '',
+  addressOne: '',
+  addressTwo: '',
+  addressThree: '',
+  firstName: '',
+  lastName: '',
+  email: ''
+}
+const UPDATE_LOAN_TYPE = 'UPDATE_LOAN_TYPE';
+const UPDATE_PROPERTY_TYPE = 'UPDATE_PROPERTY_TYPE';
+
+function loanType(loanType){
+  return {
+    type: UPDATE_LOAN_TYPE,
+    payload: loanType,
+  }
+}
+
+function property(property){
+  return {
+    type: UPDATE_PROPERTY_TYPE,
+    payload: property
+  }
+}
+
+
+function reducer(state = initialState, action){
+  switch (action.type){
+    case UPDATE_LOAN_TYPE:
+      return Object.assign({}, state, {loanType: action.payload})
+    case UPDATE_PROPERTY_TYPE:
+      return Object.assign({}, state, {propertyType: action.payload})
+    default : return state
+  }
 
 }
 
